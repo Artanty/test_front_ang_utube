17f652aa1fcce6024a95071bfa032a1f58730ab2
Artanty <thomasway7@gmail.com>
2021-09-22 23:11:09 +0300

    update angular cli to 8.1 create structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'search', loadChildren: () => import('./pages/search/search.module').then(m => m.SearchModule) }, { path: 'video', loadChildren: () => import('./pages/video/video.module').then(m => m.VideoModule) }, { path: 'comments', loadChildren: () => import('./pages/comments/comments.module').then(m => m.CommentsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
