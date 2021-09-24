4949d1d8d9b81311c9b9d7b81c1a24849d871bcf
Artanty <thomasway7@gmail.com>
2021-09-24 09:08:40 +0300

    redirect fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '',   redirectTo: 'search', pathMatch: 'full' },
   { path: 'search', loadChildren: () => import('./pages/search/search.module').then(m => m.SearchModule) },
   { path: 'video/:id', loadChildren: () => import('./pages/video/video.module').then(m => m.VideoModule) },
-  { path: 'comments/:id', loadChildren: () => import('./pages/comments/comments.module').then(m => m.CommentsModule) }
+  { path: 'comments/:id', loadChildren: () => import('./pages/comments/comments.module').then(m => m.CommentsModule) },
+  { path: '**', redirectTo: 'search' },
   ];
 
 @NgModule({
